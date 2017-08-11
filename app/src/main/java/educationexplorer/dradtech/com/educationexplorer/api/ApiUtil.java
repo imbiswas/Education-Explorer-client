b051614ba61e186e4fb7e79e5c250265b09df146
imbiswas <biswas.shre@hotmail.com>
2017-08-11 13:45:36 +0545

    mid defense commit

--- app/src/main/java/educationexplorer/dradtech/com/educationexplorer/api/ApiUtil.java
+++ app/src/main/java/educationexplorer/dradtech/com/educationexplorer/api/ApiUtil.java
@@ -8,8 +8,8 @@ public class ApiUtil {
 
     private ApiUtil(){}
 
-    public static final String BASE_URL = "http://192.168.1.27:5555/";
-    //public static final String BASE_URL = " http://192.168.0.125:5555";
+    //public static final String BASE_URL = "http://192.168.1.27:5555/";
+    public static final String BASE_URL = " http://192.168.0.171:5555";
 
     public static ApiService getApiService(){
         return RetrofitClient.getClient(BASE_URL).create(ApiService.class);
